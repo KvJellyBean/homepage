5a43ddbcf8e7a6724d74a5eda6ab761646034f48
KvJellyBean <kevinnatan91@gmail.com>
2024-02-14 09:42:20 +0700

    Add project list

---
+++ src/components/projects.js
@@ -0,0 +1,59 @@
+import jellist from '../images/jellist.png';
+import weathify from '../images/weathify.png';
+import jellySushi from '../images/jellySushi.png';
+import jellybrary from '../images/jellybrary.png';
+import calculator from '../images/calculator.png';
+import etchSketch from '../images/etchSketch.png';
+
+const projects = [
+  {
+    name: 'Jellist',
+    description:
+      'A sleek and intuitive task management tool designed to effortlessly organize your daily responsibilities, ensuring you stay productive and in control.',
+    image: jellist,
+    repo: 'https://github.com/KvJellyBean/todo-app',
+    live: 'https://kvjellybean.github.io/todo-app/'
+  },
+  {
+    name: 'Weathify',
+    description:
+      'A responsive weather application delivering up-to-the-minute conditions and detailed hourly and daily forecasts for the entire week.',
+    image: weathify,
+    repo: 'https://github.com/KvJellyBean/weather-app',
+    live: 'https://kvjellybean.github.io/weather-app/'
+  },
+  {
+    name: 'Jelly Sushi',
+    description:
+      'Responsive web app for a restaurant with a user-friendly homepage, menu for orders, and contact page—enabling accessibility on any device.',
+    image: jellySushi,
+    repo: 'https://github.com/KvJellyBean/restaurant',
+    live: 'https://kvjellybean.github.io/restaurant/'
+  },
+  {
+    name: 'Jellybrary',
+    description:
+      'A responsive web design Library project that facilitating intuitive book storage and management.',
+    image: jellybrary,
+    repo: 'https://github.com/KvJellyBean/Jellybrary',
+    live: 'https://kvjellybean.github.io/Jellybrary/'
+  },
+  {
+    name: 'Calculator',
+    description:
+      'A sleek and intuitive task management tool designed to effortlessly organize your daily responsibilities, ensuring you stay productive and in control.',
+    image: calculator,
+    repo: 'https://github.com/KvJellyBean/calculator',
+    live: 'https://kvjellybean.github.io/calculator/'
+  },
+  {
+    name: 'Etch-A-Sketch',
+    description:
+      'A sleek and intuitive task management tool designed to effortlessly organize your daily responsibilities, ensuring you stay productive and in control.',
+    image: etchSketch,
+    repo: 'https://github.com/KvJellyBean/etch-a-sketch',
+    live: 'https://kvjellybean.github.io/etch-a-sketch/'
+  }
+];
+
+export default projects;
