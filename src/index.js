477f231bc14a4b75caccc08acd91f5afa3c63431
KvJellyBean <kevinnatan91@gmail.com>
2024-02-15 16:21:02 +0700

    feat: Initialize application setup
    This commit initializes the application setup by creating header, main content, and footer elements in the DOM. It also initializes the components: Header, MainContent, and Footer.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import Footer from './components/footer';
+import Header from './components/header';
+import MainContent from './components/mainContent';
 import './style.css';
+
+function initApp() {
+  const bodyTag = document.body;
+  const headerTag = document.createElement('header');
+  const mainTag = document.createElement('main');
+  const footerTag = document.createElement('footer');
+
+  bodyTag.append(headerTag, mainTag, footerTag);
+
+  Header.initHeader();
+  MainContent.initMain();
+  Footer.initFooter();
+}
+
+initApp();
